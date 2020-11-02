d4ef0e6ecebb8fb7662d5eb27cd0249dec16b444
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2020-11-02 13:43:25 -0600

    Proper homepage entry in package.json...
    I'm dumb : - )

---
+++ docs/precache-manifest.df1468ab421e00d1425c88db32d42482.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94537fa5cbba5afaa474bd930873ab2b",
+    "url": "/notesapp/index.html"
+  },
+  {
+    "revision": "23b9b10c18c74c04379f",
+    "url": "/notesapp/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "a3d813e7ef5e6123c619",
+    "url": "/notesapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "23b9b10c18c74c04379f",
+    "url": "/notesapp/static/js/2.13bf3f25.chunk.js"
+  },
+  {
+    "revision": "7c68b657c7cc254f4ca9b56af0cb6fab",
+    "url": "/notesapp/static/js/2.13bf3f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3d813e7ef5e6123c619",
+    "url": "/notesapp/static/js/main.f01838fa.chunk.js"
+  },
+  {
+    "revision": "62ccf5cbe480d591582e",
+    "url": "/notesapp/static/js/runtime-main.d30671ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/notesapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/noteapp/precache-manifest.82c74611e5f9e632513b443bd39432b2.js"
+  "/notesapp/precache-manifest.df1468ab421e00d1425c88db32d42482.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/noteapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/notesapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
