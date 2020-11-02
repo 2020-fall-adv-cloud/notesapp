339f010f9d7e815c97a6f1ba6d4fa423f5f4a67e
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2020-11-02 13:29:52 -0600

    Attempting to republish to GitHub pages...
    Turns we need a homepage entry in the package.json...

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.480762edddf12b12b0e82de157cb3e11.js"
+  "/noteapp/precache-manifest.82c74611e5f9e632513b443bd39432b2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/noteapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
